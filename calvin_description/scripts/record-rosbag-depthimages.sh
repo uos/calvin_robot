810ad1488509dc3f6543d1ca0a76f545227d4484
André Potenza <apotenza@uos.de>
2013-08-14 13:06:04 +0200

    Added script for recording a rosbag

---
+++ calvin_description/scripts/record-rosbag-depthimages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -i
+rosbag record /tf /scan /joint_states /kinect/depth_registered/image /kinect/rgb/camera_info /kinect/depth_registered/camera_info /kinect/rgb/image_color /imu/data /odom /vgacam/camera_info /vgacam/image_raw
+
+# Notes:
+# * raw rgb + depth images         -- ca.  2 MB/frame --> up to 15 Hz
+# * kinect/depth_registered/points -- ca. 10 MB/frame --> up to  3 Hz
+# * include openni driver like this:
+#      <include file="$(find openni_launch)/launch/openni.launch">
+#        <arg name="camera" value="kinect" />
+#        <arg name="publish_tf" value="false" />
+#        <arg name="depth_registration" value="true" />
+#      </include>
+#      <param name="/kinect/driver/data_skip" value="2" />   # number of frames to skip, i.e. 0 = 30 Hz, 1 = 15 Hz, ..., 10 = 3 Hz
