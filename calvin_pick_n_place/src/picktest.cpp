077ffe58d553f1e5a286da255f72c46940500b81
Michael Stypa <mstypa@uos.de>
2013-12-17 22:41:54 +0100

    picktest update

--- calvin_pick_n_place/src/picktest.cpp
+++ calvin_pick_n_place/src/picktest.cpp
@@ -10,7 +10,7 @@
 
 #include <object_recognition_msgs/TableArray.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <moveit_msgs/CollisionObject.h>
+//#include <moveit_msgs/CollisionObject.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "opencv2/highgui/highgui.hpp"
 
@@ -25,17 +25,149 @@ class PickPlaceGroup
   {
   }
 
+moveit_msgs::Grasp graspmult(moveit_msgs::Grasp a, moveit_msgs::Grasp b) {
+moveit_msgs::Grasp x;
+  geometry_msgs::PoseStamped c;
+  c.header.frame_id = "base_footprint";
+  c.pose.position.x = a.grasp_pose.pose.position.x + b.grasp_pose.pose.position.x;
+  c.pose.position.y = a.grasp_pose.pose.position.y + b.grasp_pose.pose.position.y;
+  c.pose.position.z = a.grasp_pose.pose.position.z + b.grasp_pose.pose.position.z;
+  c.pose.orientation.x = a.grasp_pose.pose.orientation.x + b.grasp_pose.pose.orientation.x;
+  c.pose.orientation.y = a.grasp_pose.pose.orientation.y + b.grasp_pose.pose.orientation.y;
+  c.pose.orientation.z = a.grasp_pose.pose.orientation.z + b.grasp_pose.pose.orientation.z;
+  c.pose.orientation.w = 1;
+x.grasp_pose = c;
+
+
+      x.pre_grasp_approach.direction.vector.z = 1.0;
+      x.pre_grasp_approach.direction.header.frame_id = "katana_gripper_tool_frame";
+      x.pre_grasp_approach.min_distance = 0.1;
+      x.pre_grasp_approach.desired_distance = 0.2;
+
+      x.post_grasp_retreat.direction.header.frame_id = "base_footprint";
+      x.post_grasp_retreat.direction.vector.z = 1.0;
+      x.post_grasp_retreat.min_distance = 0.2;
+      x.post_grasp_retreat.desired_distance = 0.3;
+
+      x.pre_grasp_posture.joint_names.resize(1, "katana_r_finger_joint");
+      x.pre_grasp_posture.points.resize(1);
+      x.pre_grasp_posture.points[0].positions.resize(1);
+      x.pre_grasp_posture.points[0].positions[0] = 1;
+
+      x.grasp_posture.joint_names.resize(1, "katana_r_finger_joint");
+      x.grasp_posture.points.resize(1);
+      x.grasp_posture.points[0].positions.resize(1);
+      x.grasp_posture.points[0].positions[0] = 0;
+
+return x;
+}
+
+/**
+ * x, y, z: center of grasp point (the point that should be between the finger tips of the gripper)
+ */                                                                                
+std::vector<moveit_msgs::Grasp> generate_grasps(double x, double y, double z)
+{                                                                                  
+  static const double ANGLE_INC = M_PI / 16;                                       
+  static const double STRAIGHT_ANGLE_MIN = 0.0 + ANGLE_INC;  // + ANGLE_INC, because 0 is already covered by side grasps
+  static const double ANGLE_MAX = M_PI / 2;                                        
+                                                                                   
+  // how far from the grasp center should the wrist be?                            
+  static const double STANDOFF = -0.12;                                            
+                                                                                   
+  std::vector<moveit_msgs::Grasp> grasps;                                               
+                                                                                   
+  moveit_msgs::Grasp transform;                                                         
+                                                                                   
+  moveit_msgs::Grasp standoff_trans;                                                    
+  geometry_msgs::PoseStamped p0;
+  p0.header.frame_id = "base_footprint";
+  p0.pose.position.x = STANDOFF;
+  p0.pose.position.y = 0.0;
+  p0.pose.position.z = 0.0;
+  p0.pose.orientation.x = 0;
+  p0.pose.orientation.y = 0;
+  p0.pose.orientation.z = 0;
+  p0.pose.orientation.w = 1;
+  standoff_trans.grasp_pose = p0;
+                                                                                   
+                                                                                   
+  // ----- side grasps                                                             
+  //                                                                               
+  //  1. side grasp (xy-planes of `katana_motor5_wrist_roll_link` and of `katana_base_link` are parallel):
+  //     - standard: `rpy = (0, 0, *)` (orientation of `katana_motor5_wrist_roll_link` in `katana_base_link` frame)
+  //     - overhead: `rpy = (pi, 0, *)`                                            
+  geometry_msgs::PoseStamped p1;
+  p1.header.frame_id = "base_footprint";
+  p1.pose.position.x = x;
+  p1.pose.position.y = y;
+  p1.pose.position.z = z;
+  transform.grasp_pose = p1;
+                                                                                   
+  for (double roll = 0.0; roll <= M_PI; roll += M_PI)                              
+  {                                                                                
+    double pitch = 0.0;                                                            
+                                                                                   
+    // add yaw = 0 first, then +ANGLE_INC, -ANGLE_INC, 2*ANGLE_INC, ...            
+    // reason: grasps with yaw near 0 mean that the approach is from the           
+    // direction of the arm; it is usually easier to place the object back like 
+    // this                                                                        
+    for (double yaw = ANGLE_INC; yaw <= ANGLE_MAX; yaw += ANGLE_INC)               
+    {                                                                              
+      // + atan2 to center the grasps around the vector from arm to object         
+  p1.pose.orientation.x = roll;
+  p1.pose.orientation.y = pitch;
+  p1.pose.orientation.z = yaw + atan2(y, x);
+  p1.pose.orientation.w = 1;
+      grasps.push_back(graspmult(transform, standoff_trans));                                
+                                                                                   
+      if (yaw != 0.0)                                                              
+      {                                                                            
+  p1.pose.orientation.x = roll;
+  p1.pose.orientation.y = pitch;
+  p1.pose.orientation.z = -yaw + atan2(y, x);
+  p1.pose.orientation.w = 1;
+        grasps.push_back(graspmult(transform, standoff_trans));                              
+      }                                                                            
+    }                                                                              
+  }                                                                                
+                                                                                   
+  // ----- straight grasps                                                         
+  //                                                                               
+  //  2. straight grasp (xz-plane of `katana_motor5_wrist_roll_link` contains z axis of `katana_base_link`)
+  //     - standard: `rpy = (0, *, atan2(y_w, x_w))`   (x_w, y_w = position of `katana_motor5_wrist_roll_link` in `katana_base_link` frame)
+  //     - overhead: `rpy = (pi, *, atan2(y_w, x_w))`                              
+  for (double roll = 0.0; roll <= M_PI; roll += M_PI)                              
+  {                                                                                
+    for (double pitch = STRAIGHT_ANGLE_MIN; pitch <= ANGLE_MAX; pitch += ANGLE_INC)
+    {                                                                              
+      double yaw = atan2(y, x);                                                    
+  p1.pose.position.x = x;
+  p1.pose.position.y = y;
+  p1.pose.position.z = z;
+  p1.pose.orientation.x = roll;
+  p1.pose.orientation.y = pitch;
+  p1.pose.orientation.z = yaw;
+  p1.pose.orientation.w = 1;
+                                                                                   
+      grasps.push_back(graspmult(transform, standoff_trans));                                
+    }                                                                              
+  }                                                                                
+                                                                                   
+  return grasps;                                                                   
+}   
+
     bool pick(const std::string &object)
     {
+
       std::vector<moveit_msgs::Grasp> grasps;
 
       geometry_msgs::PoseStamped p;
       moveit_msgs::Grasp g;
 
       p.header.frame_id = "base_footprint";
-      p.pose.position.x = 0.5;
+      p.pose.position.x = 0.35;
       p.pose.position.y = 0.0;
-      p.pose.position.z = 1.0;
+      p.pose.position.z = 0.85;
       p.pose.orientation.x = 0;
       p.pose.orientation.y = 0;
       p.pose.orientation.z = 0;
@@ -44,13 +176,13 @@ class PickPlaceGroup
 
       g.pre_grasp_approach.direction.vector.z = 1.0;
       g.pre_grasp_approach.direction.header.frame_id = "katana_gripper_tool_frame";
-      g.pre_grasp_approach.min_distance = 0.05;
-      g.pre_grasp_approach.desired_distance = 0.15;
+      g.pre_grasp_approach.min_distance = 0.1;
+      g.pre_grasp_approach.desired_distance = 0.2;
 
       g.post_grasp_retreat.direction.header.frame_id = "base_footprint";
       g.post_grasp_retreat.direction.vector.z = 1.0;
-      g.post_grasp_retreat.min_distance = 0.1;
-      g.post_grasp_retreat.desired_distance = 0.25;
+      g.post_grasp_retreat.min_distance = 0.2;
+      g.post_grasp_retreat.desired_distance = 0.3;
 
       g.pre_grasp_posture.joint_names.resize(1, "katana_r_finger_joint");
       g.pre_grasp_posture.points.resize(1);
@@ -63,7 +195,10 @@ class PickPlaceGroup
       g.grasp_posture.points[0].positions[0] = 0;
 
       grasps.push_back(g);
+
       //group.setSupportSurfaceName("table");
+
+      //return group_.pick(object, generate_grasps(0.45, 0.0, 0.85));
       return group_.pick(object, grasps);
     }
 
@@ -75,8 +210,8 @@ class PickPlaceGroup
 int main(int argc, char **argv)
 {
   ros::init (argc, argv, "calvin_picktest");
-  ros::AsyncSpinner spinner(1);
-  spinner.start();
+  //ros::AsyncSpinner spinner(1);
+  //spinner.start();
 
   ros::NodeHandle nh;
 
@@ -101,9 +236,9 @@ int main(int argc, char **argv)
   co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = 0.04;
   co.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = 0.055;
   co.primitive_poses.resize(1);
-  co.primitive_poses[0].position.x = 0.5;
+  co.primitive_poses[0].position.x = 0.45;
   co.primitive_poses[0].position.y = 0.0;
-  co.primitive_poses[0].position.z = 1.0;
+  co.primitive_poses[0].position.z = 0.85;
   co.primitive_poses[0].orientation.w = 1.0;
 
   // wait a bit for ros things to initialize
@@ -116,16 +251,10 @@ int main(int argc, char **argv)
   pose_store[2] = -1.0093;
   pose_store[3] = 0.6966;
   pose_store[4] = -2.7886;
-  //joint_values[0] = -2.01;
-  //joint_values[1] = 0.737;
-  //joint_values[2] = -2.00;
-  //joint_values[3] = -2.00;
-  //joint_values[4] = -2.5774;
-  //joint_values[5] = -1.184;
-  //joint_values[6] = -1.16;
-
+/*
   group.setJointValueTarget(pose_zeros);
   group.move();
+*/
 
   const static unsigned int START = 0;
   const static unsigned int PICK = 1;
